a6efd3715e1516810e0f91a9dcc37cd01880a9ef
coll254-strath <149684399+coll254-strath@users.noreply.github.com>
2024-02-25 18:12:46 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div> 
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+ <h1 className='font-bold text-lg text-orange-400'>
+   Welcome to tailwind</h1>
+    </div>
+  )
+}
+
+export default App;
